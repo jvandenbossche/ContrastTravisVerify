000f39e7368208ab359ff4bf1ff2881dc0c253d9
vandenbo <50995298+jvandenbossche@users.noreply.github.com>
2019-08-20 12:01:29 -0400

    Add files via upload

--- ContrastVerify.sh
+++ ContrastVerify.sh
@@ -1,7 +1,7 @@
 echo ""
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
 echo "Contrast Security - Travis Build Verification"
-echo "Version 2019_07_18"
+echo "PlugIn Integration - Version 2019_08_20"
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-+"
 
 CONTRAST_URL=$TRAVIS_ENV_CONTRAST_TEAMSERVERURL
@@ -12,17 +12,18 @@ CONTRAST_APP_ID=$TRAVIS_ENV_CONTRAST_APPID
 
 echo "Team Server URL: $CONTRAST_URL"
 
-# [  ] USER CONFIGURATIONS
 # SET THRESHOLD MAXIMUMS FOR EACH VULNERABILITY SEVERITY TYPE
 CONTRAST_CRITICAL_COUNT=0
 CONTRAST_HIGH_COUNT=0
-CONTRAST_MEDIUM_COUNT=1
-CONTRAST_LOW_COUNT=1
-CONTRAST_NOTE_COUNT=3
+CONTRAST_MEDIUM_COUNT=0
+CONTRAST_LOW_COUNT=0
+CONTRAST_NOTE_COUNT=0
+
+# IMPORT SETTINGS FROM "ContrastTravis.conf"
 
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-"
-echo "Travis Build Vulnerability Threshold Settings"
-echo "If current open vulnerabilities in Contrast Security exceeds thresholds, the build will be failed"
+echo "IBM Travis Build Vulnerability Threshold Settings"
+echo "If current open vulnerabilities exceeds thresholds, the build will be failed"
 echo "Critical > $CONTRAST_CRITICAL_COUNT"
 echo "High     > $CONTRAST_HIGH_COUNT"
 echo "Medium   > $CONTRAST_MEDIUM_COUNT"
